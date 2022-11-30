192971e013560dead91a757e9e3d26190aba7e57
timfmb09 <tim.fmb@gmail.com>
2022-12-01 00:23:05 +0100

    Creation of relation ongoing

---
+++ src/Entity/Module.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModuleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ModuleRepository::class)
+ */
+class Module
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomModule;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomModule(): ?string
+    {
+        return $this->nomModule;
+    }
+
+    public function setNomModule(string $nomModule): self
+    {
+        $this->nomModule = $nomModule;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Session.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SessionRepository::class)
+ */
+class Session
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nomSession;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateFin;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbPlace;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Formateur::class, inversedBy="sessions")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $formateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomSession(): ?string
+    {
+        return $this->nomSession;
+    }
+
+    public function setNomSession(string $nomSession): self
+    {
+        $this->nomSession = $nomSession;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getNbPlace(): ?int
+    {
+        return $this->nbPlace;
+    }
+
+    public function setNbPlace(int $nbPlace): self
+    {
+        $this->nbPlace = $nbPlace;
+
+        return $this;
+    }
+
+    public function getFormateur(): ?Formateur
+    {
+        return $this->formateur;
+    }
+
+    public function setFormateur(?Formateur $formateur): self
+    {
+        $this->formateur = $formateur;
+
+        return $this;
+    }
+}
